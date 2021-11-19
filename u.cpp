247bf57cdfb974297ac6ad3876a9a791726bdf8b
Artem1KN <91216403+Artem1KN@users.noreply.github.com>
2021-11-19 23:18:42 +0500

    Add files via upload

---
+++ u.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int reverse(int n, int a = 0)
+{
+    if (n == 0) {
+        return a;
+    }
+    else {
+        return reverse(n / 10, 10 * a + n % 10);
+    }
+}
+
+int main()
+{
+    int c;
+    cin >> c;
+    cout << reverse(c);
+}
